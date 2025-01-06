017a8529141f7bed73f6cb18886669ce0d17a5e8
KrishnaVarshitaBorra <krishnavarshitaborra3@gmail.com>
2025-01-06 22:08:55 +0530

    assignment1.2 commit

---
+++ assignment1/question2.py
@@ -0,0 +1,10 @@
+def process(status):
+    status_actions = {
+        'active': 'Active',
+        'inactive': 'Inactive'
+    }
+    print(status_actions.get(status, 'Unknown'))
+
+data = {'status': 'active'}
+process(data['status'])
+
